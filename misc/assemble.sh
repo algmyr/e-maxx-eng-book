ae429077f93540dbe94edffac278f60dfbcf2674
Anton Älgmyr <algmyr@student.chalmers.se>
2018-10-21 00:06:50 +0200

    Prepare for the subsectioning

--- misc/assemble.sh
+++ misc/assemble.sh
@@ -6,7 +6,9 @@ cd `dirname "$0"`/..
 TEMPFILE=$(mktemp) && (
 while read line; do
     if [[ "$line" == "###"* ]]; then
-        echo "$line" | sed -E 's/### (.*)/\\chapter{\1}/' >> $TEMPFILE
+        echo "$line" | sed -E 's/### (.*)/\\part{\1}/' >> $TEMPFILE
+    elif [[ "$line" == "- **"* ]]; then
+        echo "$line" | sed -E 's/- \*\*(.*)\*\*/\\chapter{\1}/' >> $TEMPFILE
     elif [[ "$line" == "- "* ]]; then
         cat $(echo "$line" | sed -E 's!- \[.*\]\(./(.*).html\)!e-maxx-eng/src/\1.tex!') >> $TEMPFILE
     fi
